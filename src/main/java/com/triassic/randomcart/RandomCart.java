957d45df48858e2ecd2197c2a5a1c25ede9cda4b
RealTriassic <techbyteofficial9@gmail.com>
2022-12-29 20:03:10 +0200

    First Release

---
+++ src/main/java/com/triassic/randomcart/RandomCart.java
@@ -0,0 +1,79 @@
+package com.triassic.randomcart;
+
+import com.triassic.randomcart.listeners.BlockBreakListener;
+import org.bukkit.Material;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
+
+public final class RandomCart extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Logger logger = getLogger();
+        Configuration config = getConfig();
+        Random random = new Random();
+
+        this.saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(config, logger, random), this);
+    }
+
+    public static class Minecart {
+        private final String name;
+        private final List<String> items;
+        private final String selectedMinecart;
+
+        public Minecart (String name, List<String> items, String selectedMinecart) {
+            this.name = name;
+            this.items = items;
+            this.selectedMinecart = selectedMinecart;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<String> getItems() {
+            return items;
+        }
+
+        public String getSelectedMinecart() {
+            return selectedMinecart;
+        }
+    }
+
+    public static Minecart loadMinecart(Configuration config, Random random) {
+        List<String> minecartKeys = new ArrayList<>(config.getConfigurationSection("minecarts").getKeys(false));
+        String selectedMinecart = minecartKeys.get(random.nextInt(minecartKeys.size()));
+
+        String name = config.getString("minecarts." + selectedMinecart + ".name");
+
+        ConfigurationSection itemsConfig = config.getConfigurationSection("minecarts." + selectedMinecart + ".items");
+        List<String> items = new ArrayList<>(itemsConfig.getKeys(false));
+
+        return new Minecart(name, items, selectedMinecart);
+    }
+
+    public static List<Material> loadAllowedBlocks(Configuration config, Logger logger) {
+        List<String> allowedBlockNames = config.getStringList("randomcart.allowed-blocks");
+        List<Material> allowedBlocks = new ArrayList<>();
+
+        for (String blockName : allowedBlockNames) {
+            Material blockMaterial = Material.getMaterial(blockName);
+            if (blockMaterial == null) {
+                logger.warning("Invalid block name: " + blockName + " in allowed blocks list");
+                continue;
+            }
+
+            allowedBlocks.add(blockMaterial);
+        }
+
+        return allowedBlocks;
+    }
+}
---
+++ src/main/java/com/triassic/randomcart/listeners/BlockBreakListener.java
@@ -0,0 +1,111 @@
+package com.triassic.randomcart.listeners;
+
+import com.triassic.randomcart.RandomCart;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.minecart.StorageMinecart;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
+
+public class BlockBreakListener implements Listener {
+    private final Logger logger;
+    private final Random random;
+    private final Configuration config;
+    private final List<Material> allowedBlocks;
+    private final int summonChance;
+
+    public BlockBreakListener(Configuration config, Logger logger, Random random) {
+        this.config = config;
+        this.logger = logger;
+        this.random = random;
+        this.allowedBlocks = RandomCart.loadAllowedBlocks(config, logger);
+        this.summonChance = config.getInt("randomcart.chance");
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        if (!allowedBlocks.contains(block.getType())) {
+            return;
+        }
+
+        if (random.nextInt(100) > summonChance) {
+            return;
+        }
+
+        Location blockLocation = block.getLocation();
+        blockLocation.setY(blockLocation.getY() + 0.75);
+
+        StorageMinecart chestMinecart = (StorageMinecart) block.getWorld().spawnEntity(blockLocation, EntityType.MINECART_CHEST);
+        Inventory chestInventory = chestMinecart.getInventory();
+
+        RandomCart.Minecart minecartData = RandomCart.loadMinecart(config, random);
+        String selectedMinecart = minecartData.getSelectedMinecart();
+        String chestName = ChatColor.translateAlternateColorCodes('&', minecartData.getName());
+        chestMinecart.setCustomName(chestName);
+
+        for (String itemString : minecartData.getItems()) {
+            Material material = Material.getMaterial(itemString);
+            if (material == null) {
+                logger.warning("Invalid item name " + itemString + " in " + selectedMinecart);
+                continue;
+            }
+
+            String displayName = config.getString("minecarts." + selectedMinecart + ".items." + itemString + ".display-name");
+            int stackSize = config.getInt("minecarts." + selectedMinecart + ".items." + itemString + ".amount");
+            List<String> lore = config.getStringList("minecarts." + selectedMinecart + ".items." + itemString + ".lore");
+            ConfigurationSection enchantmentsSection = config.getConfigurationSection("minecarts." + selectedMinecart + ".items." + itemString + ".enchantments");
+
+            if (stackSize == 0) {
+                stackSize = 1;
+            }
+
+            ItemStack item = new ItemStack(material, stackSize);
+            ItemMeta meta = item.getItemMeta();
+
+            if (displayName != null) {
+                meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', displayName));
+            }
+
+            if (lore != null) {
+                List<String> coloredLore = new ArrayList<>();
+                for (String line : lore) {
+                    coloredLore.add(ChatColor.translateAlternateColorCodes('&', line));
+                }
+
+                meta.setLore(coloredLore);
+            }
+
+            if (enchantmentsSection != null) {
+                for (String ench : enchantmentsSection.getKeys(false)) {
+                    Enchantment enchantment = Enchantment.getByKey(NamespacedKey.minecraft(ench.toLowerCase()));
+                    int level = enchantmentsSection.getInt(ench + ".level");
+                    try {
+                        meta.addEnchant(enchantment, level, true);
+                    } catch (IllegalArgumentException e) {
+                        logger.warning("Invalid enchantment name " + ench + " in " + selectedMinecart);
+                    }
+                }
+            }
+
+            item.setItemMeta(meta);
+            chestInventory.addItem(item);
+        }
+    }
+}
